874ef3eb543997c9da02243b15d507698a7675ae
vakiln <nusli.vakil@autodesk.com>
2017-10-24 23:14:21 -0700

    Fixed bug. Now tetBox is working

--- WorkInProgress/changeRequest.js
+++ WorkInProgress/changeRequest.js
@@ -1,13 +1,15 @@
-function validateTextArea() {
-    if ($(this).children("textarea")[0].value.length == 0) { // text area is empty
-        $(this).children("p")[0].innerHTML = "Please input valid input";
-    }
-}
+$(document).ready(function () {
 
-$("input#submitButton").click(function () {
-    //   var element = $("#debug");
-    //    validateTextArea(element);
-    
-    //$("div.textAreaInput").forEach(validateTextArea($(this)))
-    $("div.textAreaInput").each( validateTextArea );
+    // Function to validate Text Areas
+    // It expects $(this) to have a div containig textbox
+    function validateTextArea() {
+        if ($(this).children("textarea")[0].value.length == 0) { // text area is empty
+            $(this).children("p")[0].innerHTML = "Please input valid input";
+        }
+    }
+
+    $("input#submitButton").click(function () {
+        $("div.textAreaInput").each(validateTextArea);
+    });
+
 });
