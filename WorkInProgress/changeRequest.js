155c04a94a571d071f7934990962dd604718948e
vakiln <nusli.vakil@autodesk.com>
2017-10-24 23:38:29 -0700

    Added checkbox validation

--- WorkInProgress/changeRequest.js
+++ WorkInProgress/changeRequest.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
-
+    
     // Function to validate Text Areas
     // It expects $(this) to have a div containig textbox
     function validateTextArea() {
+        console.log($(this));
         if ($(this).children("textarea")[0].value.length == 0) { // text area is empty
             $(this).children("p")[0].innerHTML = "Please input valid input";
         }
     }
-
+    
+    function validateCheckbox(){
+        console.log("hello");
+        $(this).find('input[type="checkbox" input:checked]').length == 0{
+            $(this).children("p")[0].innerHTML = "Please check least one box";
+        }
+    }
+    
     $("input#submitButton").click(function () {
         $("div.textAreaInput").each(validateTextArea);
+        $("div.checkboxInput").each(validateCheckbox);
     });
-
+    
 });
